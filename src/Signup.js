41d4098803db5e202cb644b7ea4e2d3b2e7e9850
James 'Tucker' Wray <tuckerwray84@gmail.com>
2020-12-01 13:49:51 -0500

    initial commit

--- src/Signup.js
+++ src/Signup.js
@@ -1,5 +1,35 @@
 import React from "react";
+import SignupForm from "./Signup/SignupForm";
+
+const left = {
+  display: "flex",
+  width: "50%",
+  border: "solid pink 1rem",
+  height: "100%"
+};
+const right = {
+  display: "flex",
+  width: "50%",
+  border: "solid pink 1rem",
+  height: "100%"
+};
 
 export default function Signup() {
-  return <>register here form inputs button</>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "99vh",
+        padding: "0",
+        border: "red solid 1rem",
+        fontSize: "3rem"
+      }}
+    >
+      <div className="half left image" style={left}></div>
+      <div className="half right form" style={right}>
+        <SignupForm />
+      </div>
+    </div>
+  );
 }
---
+++ src/Signup/SignupForm.tsx
@@ -0,0 +1,43 @@
+import React, { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import ReCAPTCHA from "react-google-recaptcha";
+
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+  terms: boolean;
+  d;
+}
+
+export default function SignupForm() {
+  const { register, handleSubmit } = useForm<FormData>();
+
+  return (
+    <form
+      onSubmit={handleSubmit((formData) => {
+        console.log("formData", formData);
+      })}
+    >
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" ref={register()} />
+      </div>
+      <div>
+        <label htmlFor="name">Email</label>
+        <input type="email" name="email" id="email" ref={register()} />
+      </div>
+      <div>
+        <label htmlFor="pasword">Password</label>
+        <input type="password" name="password" id="password" ref={register()} />
+      </div>
+      <div>
+        <label htmlFor="terms">Remember Me</label>
+        <input type="checkbox" name="terms" id="terms" ref={register()} />
+      </div>
+      <div>
+        <button type="submit">Signup</button>
+      </div>
+    </form>
+  );
+}
