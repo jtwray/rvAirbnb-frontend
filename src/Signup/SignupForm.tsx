c7e2a05bbd0e9738909de215a77459a2aff48718
James 'Tucker' Wray <tuckerwray84@gmail.com>
2020-12-01 18:42:11 -0500

    feat-SignupForm-styles - floating labels on input focus - recreates MaterialUI transitioning labels with vanilla css

--- src/Signup/SignupForm.tsx
+++ src/Signup/SignupForm.tsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { useForm } from "react-hook-form";
 import ReCAPTCHA from "react-google-recaptcha";
 
@@ -7,7 +8,6 @@ interface FormData {
   email: string;
   password: string;
   terms: boolean;
-  d;
 }
 
 export default function SignupForm() {
@@ -15,65 +15,69 @@ export default function SignupForm() {
   const [submitting, setSubmitting] = useState<boolean>(false);
 
   return (
-    <form
-      style={{ fontSize: "4rem" }}
-      onSubmit={handleSubmit((formData) => {
-        setSubmitting(true);
-        console.log("formData", formData);
-        setSubmitting(false);
-      })}
-    >
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          ref={register({ required: "required" })}
-        />
-        {errors.name ? <p> what's your username? </p> : null}
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          ref={register({ required: "required" })}
-        />
-        {errors.email ? <p> provide an email </p> : null}
-      </div>
-      <div>
-        <label htmlFor="pasword">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          ref={register({
-            required: "required",
-            minLength: {
-              value: 8,
-              message:
-                "must be 8 chars. Try using an unintelligible phrase like  ~ your-horse-battery-eats-staples-walmart ~"
-            },
-            validate: (value) =>
-              [
-                /^((?!your-horse-battery-eats-staples-walmart).)*$/
-              ].every((pattern) => pattern.test(value)) ||
-              "Try using an unintelligible phrase other than ~ your-horse-battery-eats-staples-walmart ~ "
-          })}
-        />
-        {errors.password ? <p>{errors.password.message} </p> : null}
-      </div>
-      <div>
-        <label htmlFor="terms">Remember Me</label>
-        <input type="checkbox" name="terms" id="terms" ref={register()} />
-      </div>
-      <div>
-        <button type="submit" disabled={submitting}>
-          Signup
-        </button>
-      </div>
-    </form>
+    <>
+      {/* <span style={{position:"relative",top:".15%",left:"35%",background:"white",height:"10px"}}>Name:</span> */}
+      <form
+        style={{ fontSize: "4rem" }}
+        onSubmit={handleSubmit(async (formData) => {
+          setSubmitting(true);
+          console.log("formData", formData);
+          setSubmitting(false);
+          const response = await axiosWithAuth().get("/auth/rv/signup");
+        })}
+      >
+        <div className="input">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            ref={register({ required: "required" })}
+          />
+          {errors.name ? <p> what's your username? </p> : null}
+        </div>
+        <div className="input">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            ref={register({ required: "required" })}
+          />
+          {errors.email ? <p> provide an email </p> : null}
+        </div>
+        <div className="input">
+          <label htmlFor="pasword">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            ref={register({
+              required: "required",
+              minLength: {
+                value: 8,
+                message:
+                  "must be 8 chars. Try using an unintelligible phrase like  ~ your-horse-battery-eats-staples-walmart ~"
+              },
+              validate: (value) =>
+                [
+                  /^((?!your-horse-battery-eats-staples-walmart).)*$/
+                ].every((pattern) => pattern.test(value)) ||
+                "Try using an unintelligible phrase other than ~ your-horse-battery-eats-staples-walmart ~ "
+            })}
+          />
+          {errors.password ? <p>{errors.password.message} </p> : null}
+        </div>
+        <div>
+          <input type="checkbox" name="terms" id="terms" ref={register()} />
+          <label htmlFor="terms">Remember Me</label>
+        </div>
+        <div>
+          <button type="submit" disabled={submitting}>
+            Signup
+          </button>
+        </div>
+      </form>
+    </>
   );
 }
