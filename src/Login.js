096596c2b5f7ae8a7ae906f5109f7d2a80e0a74b
James 'Tucker' Wray <tuckerwray84@gmail.com>
2020-12-03 11:55:32 -0500

    feat-- Login Styles &  Image - borrows styles and layout from Signup - image left half - form right half - would be good to comeback and compartmentalize the style objects later into an exportable styles file for use in both authO forms  [ Login and Signin ]

--- src/Login.js
+++ src/Login.js
@@ -3,6 +3,24 @@ import { login } from "./redux/actions/index.js";
 import { connect } from "react-redux";
 import LoginForm from "./LoginForm";
 
+const left = {
+  display: "flex",
+  width: "50%",
+  height: "100%",
+  backgroundImage: "url(https://source.unsplash.com/random)",
+  backgroundSize: "cover",
+  backgroundPosition: "center"
+};
+const right = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-evenly",
+  alignItems: "center",
+  width: "50%",
+  height: "100%",
+  boxShadow: "-1px 0px 5px .2px black"
+};
+
 function Login(props) {
   const initialCredentials = { username: "", password: "", email: "" };
   const [credentials, setCredentials] = useState(initialCredentials);
@@ -21,14 +39,26 @@ function Login(props) {
   }
 
   return (
-    <>
-      <LoginForm
-        handleSubmit={handleSubmit}
-        handleChange={handleChange}
-        credentials={credentials}
-        history={props.history}
-      />
-    </>
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "99vh",
+        padding: "0",
+        border: "red solid 1rem",
+        fontSize: "3rem"
+      }}
+    >
+      <div className="half left image" style={left}></div>
+      <div className="half right form" style={right}>
+        <LoginForm
+          handleSubmit={handleSubmit}
+          handleChange={handleChange}
+          credentials={credentials}
+          history={props.history}
+        />
+      </div>
+    </div>
   );
 }
 
