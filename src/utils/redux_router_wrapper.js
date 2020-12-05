a8775c87426ffecccb38c40ba9f558b7434dc556
tucker <tuckerwray84@gmail.com>
2020-12-05 17:26:55 -0500

    brings in 4 images demonstrating a mock for the SingleListingView

--- src/utils/redux_router_wrapper.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Provider } from "redux";
-import { createStore } from "redux";
-import { render } from "@testing-library/react";
-import { rootReducer as reducer } from "../redux/reducers/rootReducer";
-import { BrowserRouter } from "react-router-dom";
