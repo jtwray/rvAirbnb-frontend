fa867a3d59f51ee9b10f9e20c024cf96dccf18fa
tucker <tuckerwray84@gmail.com>
2020-12-05 00:00:29 -0500

    feat-- ListingCard  destructures props

--- src/ListingCard.js
+++ src/ListingCard.js
@@ -1,23 +1,33 @@
 import React from "react";
 import "./ListingCard.css";
 export default function ListingCard(props) {
+  let {
+    amenities,
+    photo,
+    location,
+    description,
+    price,
+    landowner_id,
+  } = props.listing;
+
   return (
     <>
       <div className="listingCard" key={props.key}>
         <div className="subcardGroup cardImg">
-          <img src={props.listing.photo} />
+          <img src={photo} />
         </div>
         <div className="subcardGroup cardInfo">
           <h2 className="location_zipcode">
-            Entire Apartment in zipcode: {props.listing.location}
+            Entire Apartment in zipcode: {location}
           </h2>
-          <p className="subcard_description">{props.listing.description} </p>
-          <p className="subcard_amenities">{props.listing.amenities}</p>
+          <p className="subcard_description">{description} </p>
+
+          <p className="subcard_amenities"> {amenities} </p>
         </div>
         <div className="subcardGroup CTA">
           <div className="favoriteBtn">🤍|💘</div>
-          <p className="LO_id">{props.listing.landowner_id} </p>
-          <div className="subcard_price">{props.listing.price}</div>
+          <p className="LO_id">{landowner_id} </p>
+          <div className="subcard_price">{price}</div>
         </div>
       </div>
     </>
