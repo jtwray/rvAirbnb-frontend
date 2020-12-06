2b155b867587be726f1cea75effafe770dd1d08d
tucker <tuckerwray84@gmail.com>
2020-12-06 13:49:59 -0500

    feat-- Login action race conditions  - uses async/await to counter race conditions with token storage and history.push(/)  - previous syntax login would redirect to dashboard only if a stale token was present in localstorage to be overwritten. idk  - add tests next commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,42 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import { PrivateRoute } from "./utils/PrivateRoute";
 import "./styles.css";
-import Login from "./Login";
+import Login from "./auth_views/login/Login";
 import Dashboard from "./Dashboard";
-import Logout from "./Logout";
-import Signup from "./Signup/Signup";
-
-export default function App() {
+import Logout from "./auth_views/Logout";
+import Signup from "./auth_views/signup/Signup";
+import SingleListingView from "./listings/SingleListingView";
+import Listings from "./listings/Listings";
+import { connect } from "react-redux";
+ function App(props) {
+   const {currentUser}=props
   return (
     <>
       <div className="App">
-        <PrivateRoute exact path="/" component={Dashboard} />
         {/* <PrivateRoute exact path="/*" component={Dashboard} /> */}
+console.log({currentUser})
+        <PrivateRoute exact path="/" component={Dashboard} />
+        <PrivateRoute exact path="/listings/" component={Listings} />
+        <PrivateRoute
+          exact
+          path="/listings/:listingID"
+          component={SingleListingView}
+        />
+
         <PrivateRoute exact path="/logout" component={Logout} />
-        <Route exact path="/login" component={Login} />
+        <Route
+          exact
+          path="/login"
+          component={
+            // props.currentToken && localStorage.token === props.currentToken
+            //   ? Dashboard
+            //   : 
+              Login
+          }
+        />
         <Route exact path="/signup" component={Signup} />
       </div>
     </>
   );
-}
+}function mapState({currentUser}){return{currentUser}}
+export default connect(mapState,{})(App)
