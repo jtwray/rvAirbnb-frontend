92de299a0dd399d8c53ac975075eb9e502bb302d
tucker <tuckerwray84@gmail.com>
2020-12-09 03:14:18 -0500

    feat-- Listings SingleView - very basic rendering so far - deciding how much content to deisplay on list view and what to save for the singleListing View - incorporating an Effect Hook to retrieve more information about singleListing but Im not sure we'll need anyting else from the db at this point - later wehn I get teh video calls up and messaging setup between vendor/enduser that can go here possibly? - is this the payment entry view? has the params hook from react-router-dom to grab correct Listing matching the url clicked to find this view

--- src/listings/SingleListingView.js
+++ src/listings/SingleListingView.js
@@ -1,17 +1,21 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
+export default function SingleListingView(props) {
+  console.log({ props });
+  const { listingID } = useParams();
 
-export default function SingleListingView(){
-    
-    
-    
-    
-    return(
-<div>
-    <h1>title/short description</h1>
-    
-</div>
+  useEffect(() => {
+    props.getSingleListing(listingID);
+  }, []);
+  return (
+    <div>
 
-)
-
+      <h1>title/short description</h1>
+    <h2>here's the skinny on this wonderful slice of paradise </h2>
+      "include calendar with available dates " "fetch reserved dates for this
+      month-- grey out dates booked" "include a message button to open chat with
+      vendor"
+    </div>
+  );
 }
