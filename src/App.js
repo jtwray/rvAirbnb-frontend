584ed0603e08da7eb9c90076ca6e4272c9da3581
tucker <tuckerwray84@gmail.com>
2020-12-09 02:50:38 -0500

    feat-- Login routing - cleans up login route path in App.js - removes a console.log

--- src/App.js
+++ src/App.js
@@ -8,14 +8,13 @@ import Logout from "./auth_views/Logout";
 import Signup from "./auth_views/signup/Signup";
 import SingleListingView from "./listings/SingleListingView";
 import Listings from "./listings/Listings";
-import { connect } from "react-redux";
- function App(props) {
-   const {currentUser}=props
+
+export default function App(props) {
+  const { currentUser } = props;
   return (
     <>
       <div className="App">
         {/* <PrivateRoute exact path="/*" component={Dashboard} /> */}
-console.log({currentUser})
         <PrivateRoute exact path="/" component={Dashboard} />
         <PrivateRoute exact path="/listings/" component={Listings} />
         <PrivateRoute
@@ -25,19 +24,9 @@ console.log({currentUser})
         />
 
         <PrivateRoute exact path="/logout" component={Logout} />
-        <Route
-          exact
-          path="/login"
-          component={
-            // props.currentToken && localStorage.token === props.currentToken
-            //   ? Dashboard
-            //   : 
-              Login
-          }
-        />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
       </div>
     </>
   );
-}function mapState({currentUser}){return{currentUser}}
-export default connect(mapState,{})(App)
+}
