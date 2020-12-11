ee6ec027fd2147bb10bc78598a32ecdfdda0793a
tucker <tuckerwray84@gmail.com>
2020-12-11 14:58:51 -0500

    feat-- Dashboard - removes duplications from Dashboard already in Discover - leaves in Navigation - Welcome message with currentUser from redux store - comment for geolocation ad notificationgs header - this will likely be broken down agin into separate componenets for the notifications header and geolocation

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -2,30 +2,22 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { connect } from "react-redux";
 import Navigation from "./Navigation";
-import Listings from "./listings/Listings";
+
 
 
 function Dashboard(props) {
-  // console.log({ json });
-  // const [listings, setListings] = useState(json.listings);
-  // useEffect(() => {
-  //   axios
-  //     .get("https://rventure.herokuapp.com/api/listing")
-  //     // .then((res) => setListings(res.data.listings))
-  //     .catch((e) => console.error(e));
-  // }, []);
+
   return (
     <div style={{ fontSize: "3rem" }}>
       <Navigation />
+      {/* notifications header
+      list welcome message
+      current geolocation
+      any notifications since last login
+       */}
       <h1 style={{ fontSize: "5rem" }}>welcome {props.currentUser} !</h1>
       <h2 style={{ fontSize: "5rem" }}>"current location /geolocation" !</h2>
-      <div>"search and filters"</div>
-      <div style={{width:"100%",display:"flex"}}>
-        <section style={{width:"50%"}}>
-          <Listings />
-        </section>
-        <section style={{width:"50%"}}>"MAP"</section>
-      </div>
+
     </div>
   );
 }
