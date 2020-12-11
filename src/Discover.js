0b8c2dfa83b93581a4c4f199a8cbde3ac2d820ad
tucker <tuckerwray84@gmail.com>
2020-12-11 14:55:12 -0500

    feat-- Discover.js - moved part of Dashboard into Discover.js - Listings COMponent Reused here - want to pass in the results of search /filter to Listings on props object and display that way

--- src/Discover.js
+++ src/Discover.js
@@ -1,46 +1,23 @@
 import React, { useEffect, useState } from "react";
+import Listings from "./listings/Listings";
 import axios from "axios";
 
-
-export default function Discover() {
-  return (
-    <div>
-      <h1>Discover an Adventure</h1>
-    </div>
-  );
-}
-import { connect } from "react-redux";
-import Navigation from "./Navigation";
-import Listings from "./listings/Listings";
-
-
-function Dashboard(props) {
-
+export default function Discover(props) {
   return (
     <div style={{ fontSize: "3rem" }}>
-      <Navigation />
-      {/* notifications header
-      list welcome message
-      current geolocation
-      any notifications since last login
-       */}
-      <h1 style={{ fontSize: "5rem" }}>welcome {props.currentUser} !</h1>
-      <h2 style={{ fontSize: "5rem" }}>"current location /geolocation" !</h2>
-      <div>"search and filters"
-          {/* searchbar at top under notifications header */}
+      <div>
+        <h1>Discover an Adventure</h1>
       </div>
-      <div style={{width:"100%",display:"flex"}}>
-        <section style={{width:"50%"}}>
-          <Listings props={props} />
+      <div>
+        "search and filters"
+        {/* searchbar at top under notifications header */}
+      </div>
+      <div style={{ width: "100%", display: "flex" }}>
+        <section style={{ width: "50%" }}>
+          <Listings listings={listings} />
         </section>
-        <section style={{width:"50%"}}>"MAP"</section>
+        <section style={{ width: "50%" }}>"MAPBOX"</section>
       </div>
     </div>
   );
 }
-
-function mapState(state) {
-  return { currentUser: state.currentUser };
-}
-
-export default connect(mapState, {})(Dashboard);
