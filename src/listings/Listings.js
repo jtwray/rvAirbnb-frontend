b475acaf36b58bc0f7700b033d4d4a88a523fc64
tucker <tuckerwray84@gmail.com>
2020-12-14 18:10:36 -0500

    chore- Removes axios call from Listings -listings will be reused in suggestedLIstings and searchedlistings. - LIstings is hydrated through props.listings now - called from 2 places in Discover.js

--- src/listings/Listings.js
+++ src/listings/Listings.js
@@ -1,35 +1,29 @@
 import React, { useEffect, useState } from "react";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
 import{useHistory} from "react-router-dom"
 import ListingCard from "./ListingCard";
 import { usePagination } from "../utils/usePagination.js";
 
-export default function Listings(props) {
+export default function Listings({listings}) {
   const history=useHistory()
-  const [listings, setListings] = useState([]);
+
   const [paginatedListings] = usePagination(listings, 10);
   const [page, setPage] = useState(1);
   const [pageOfListings, setPageOfListings] = useState(paginatedListings[page]);
-console.log("Listings.js line11",{props})
-  useEffect(() => {
-    axiosWithAuth()
-      .get("/api/listing")
-      .then((res) => setListings(res.data.listings))
-      .catch((e) => console.error(e));
-  }, []);
+console.log("Listings.js line11",{listings})
+
 
   useEffect(() => {
     paginatedListings && setPageOfListings(paginatedListings[page]);
-  }, [page, listings, paginatedListings]);
+  }, [page, listings, paginatedListings,]);
 
   function routeToSingleListing(event, listing) {
     event.preventDefault();
-    console.log("p.history",props,history)
+    console.log("p.history",history,"listings",listings)
  history.push(`listings/${listing.id}`,{listing:listing});
   }
 
   return (
-    <section>
+    <section id="searchedListings">
       {page > 1 && <button onClick={() => setPage(page - 1)}>⬅ back </button>}
       {page < paginatedListings.length - 1 && (
         <button onClick={() => setPage(page + 1)}> more ➡</button>
