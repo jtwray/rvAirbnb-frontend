d230695802d4ad5bcefad370165338fe6be03b26
tucker <tuckerwray84@gmail.com>
2020-12-15 15:17:16 -0500

    chore--Cleanup IMports Discover.js - comment out code relying on 3rd party api not setup yet

--- src/Discover.js
+++ src/Discover.js
@@ -39,38 +39,38 @@ export default function Discover(props) {
   }, []);
   let styleOBJ_suggested_listings={}
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-        var accuracy = position.coords.accuracy;
-        var coords = new google.maps.LatLng(latitude, longitude);
-        var mapOptions = {
-            zoom: 15,
-            center: coords,
-            mapTypeControl: true,
-            navigationControlOptions: {
-                style: google.maps.NavigationControlStyle.SMALL
-            },
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//         var latitude = position.coords.latitude;
+//         var longitude = position.coords.longitude;
+//         var accuracy = position.coords.accuracy;
+//         var coords = new google.maps.LatLng(latitude, longitude);
+//         var mapOptions = {
+//             zoom: 15,
+//             center: coords,
+//             mapTypeControl: true,
+//             navigationControlOptions: {
+//                 style: google.maps.NavigationControlStyle.SMALL
+//             },
+//             mapTypeId: google.maps.MapTypeId.ROADMAP
+//         };
 
-        var capa = document.getElementById("capa");
-        capa.innerHTML = "latitude: " + latitude + ", longitude: " + ", accuracy: " + accuracy;
+//         var capa = document.getElementById("capa");
+//         capa.innerHTML = "latitude: " + latitude + ", longitude: " + ", accuracy: " + accuracy;
 
-        map = new google.maps.Map(document.getElementById("mapContainer"), mapOptions);
-        var marker = new google.maps.Marker({
-            position: coords,
-            map: map,
-            title: "ok"
-        });
+//         map = new google.maps.Map(document.getElementById("mapContainer"), mapOptions);
+//         var marker = new google.maps.Marker({
+//             position: coords,
+//             map: map,
+//             title: "ok"
+//         });
 
-    },
-    function error(msg) {alert('Please enable your GPS position feature.');},
-    {maximumAge:10000, timeout:5000, enableHighAccuracy: true});
-} else {
-    alert("Geolocation API is not supported in your browser.");
-}
+//     },
+//     function error(msg) {alert('Please enable your GPS position feature.');},
+//     {maximumAge:10000, timeout:5000, enableHighAccuracy: true});
+// } else {
+//     alert("Geolocation API is not supported in your browser.");
+// }
 
   return (
     <div style={{ fontSize: "3rem" }}>
