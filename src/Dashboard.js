c2289bc46d8240c00ac3ffffa32167ea189c589d
tucker <tuckerwray84@gmail.com>
2020-12-15 11:49:47 -0500

    feat-- Geolocation -  dashboard sends post request to rvbnb backend as soon as BOM loads the gps coords into redux store - recieves back and logs 10 nearest street addresses to the console

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,18 +1,50 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "./utils/axiosWithAuth";
 import { connect } from "react-redux";
 import Navigation from "./Navigation";
 import { usePosition } from "./utils/usePosition";
 import { LoadingClackers } from "./utils/LoadingClackers";
+import { getCoords, updateCoords } from "./redux/actions/index";
+import { StepContent } from "@material-ui/core";
 
-function Dashboard(props) {
+function Dashboard(props, { currentGeoLocation }) {
   const [latitude, longitude, accuracy] = usePosition();
   const [gps, setGps] = useState(latitude, longitude, accuracy);
 
   useEffect(() => {
+    getCoords();
+  }, []);
+
+  useEffect(() => {
+    latitude && console.log({gps},{ latitude });
+    latitude &&
+      axiosWithAuth()
+        .post(`api/listing/geo_address`, gps)
+        .then(({ data }) => {
+          console.log({ data });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  }, [gps,latitude]);
+
+  useEffect(() => {
     setGps({ latitude, longitude, accuracy });
+    updateCoords({ latitude, longitude, accuracy });
   }, [latitude, longitude, accuracy]);
 
+  // useEffect(() => {
+  //   latitude && console.log({ latitude });
+  //   latitude &&
+  //     axiosWithAuth()
+  //       .post(`api/listing/geo_address`, {
+  //         gps,
+  //       })
+  //       .then((res) => {
+  //         console.log(res);
+  //       });
+  // }, [gps]);
   return (
     <div style={{ fontSize: "3rem" }}>
       <Navigation />
@@ -26,11 +58,11 @@ function Dashboard(props) {
         <LoadingClackers />
       ) : (
         <>
-          <h2 style={{ fontSize: "5rem" }}>
-            "current location /geolocation"
-          </h2>
+          <h2 style={{ fontSize: "5rem" }}>"current location /geolocation"</h2>
           <h6>
-          lat:{gps.latitude} lon:{gps.longitude}
+            store.lat:{currentGeoLocation?.latitude}
+            {/**  GeoLocationCoordinates.getlatitude()*/}gps.lat{gps.latitude}{" "}
+            lon:{gps.longitude}
           </h6>
         </>
       )}
@@ -39,7 +71,10 @@ function Dashboard(props) {
 }
 
 function mapState(state) {
-  return { currentUser: state.currentUser };
+  return {
+    currentUser: state.currentUser,
+    currentGeoLocation: state.currentGeoLocation,
+  };
 }
 
-export default connect(mapState, {})(Dashboard);
+export default connect(mapState, { getCoords, updateCoords })(Dashboard);
