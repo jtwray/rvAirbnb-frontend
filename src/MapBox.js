9569d49ff2d58b6b046f0882abf9024317f2ca98
tucker <tuckerwray84@gmail.com>
2020-12-15 21:23:38 -0500

    feat-- Mapboxapi displays static map - map broken out into separate component

---
+++ src/MapBox.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { connect } from "react-redux";
+
+function MapBox(props) {
+  const [viewport, setViewport] = useState({
+    latitude: props.latitude || 50,
+    longitude: props.longitude || 50,
+    width: "50vw",
+    height: "100vw",
+    zoom: 10,
+  });
+  const [selectedListing, setSelectedListing] = useState(null);
+
+  return (
+    <>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      ></ReactMapGL>
+    </>
+  );
+}
+function mapState(state) {
+  return { currentGeoLocation: state.currentGeoLocation };
+}
+
+export default connect(mapState, {})(MapBox);
