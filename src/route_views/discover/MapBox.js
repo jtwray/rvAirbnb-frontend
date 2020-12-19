d9a8a669b147de07ceba9659cecdb839fa23539d
tucker <tuckerwray84@gmail.com>
2020-12-19 00:32:41 -0500

    feat-- Markers Popup - adds button to each marker - button onclick sets active listing - triggers popup displaying active listing data title,night price,zip state city - uses 3rd party api to reduce size of SVG and to clean blocking NameSpaces - svg displays on each marker now - blockers were the namespaces not camelCased and no width set on absolute styled marker (library default styles) -lat and lon are coming down from api as string type. MapBox library popups and Markers expect int. parseing works fine when passing in the prop

--- src/route_views/discover/MapBox.js
+++ src/route_views/discover/MapBox.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
 import { LoadingClackers } from "../../utils/LoadingClackers";
 import { connect } from "react-redux";
+import { ReactComponent as RV_Site_Marker } from "../../images/image.svg";
 
 function MapBox(props) {
   const [viewport, setViewport] = useState({
@@ -23,25 +24,62 @@ function MapBox(props) {
         }}
       >
         {props.listings ? (
-          props.listings.map((listing) =>{
-  let listingLatitude=Number(listing.lat);
-  console.log({listingLatitude})
-  return (
-            <Marker
-              key={listing.id}
-              latitude={Number(listing.lon)}
-              longitude={Number(listing.lat)}
-            >
-              <button>
-                <img src="rv.svg" />
-              </button>
-            </Marker>
-          )})
+          props.listings.map((listing) => {
+            let listingLatitude = Number(listing.lat);
+
+            return (
+              <Marker
+                key={listing.id}
+                latitude={Number(listing.lon)}
+                longitude={Number(listing.lat)}
+              >
+                <button
+                  style={{
+                    background: "purple",
+                    width: "20px",
+                    height: "20px",
+                    border: "solid 1px black",
+                    cursor: "pointer",
+                    padding: "0",
+                  }}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setSelectedListing(listing);
+                  }}
+                >
+                  <RV_Site_Marker
+                    style={{
+                      width: "20px",
+                      height: "20px",
+                      padding: "0",
+                      margin: "0",
+                    }}
+                  />
+                </button>
+              </Marker>
+            );
+          })
         ) : (
           <LoadingClackers />
         )}
+        {selectedListing ? (
+          <Popup
+            latitude={Number(selectedListing.lon)}
+            longitude={Number(selectedListing.lat)}
+            onClose={() => {
+              setSelectedListing(null);
+            }}
+          >
+            <div >
+              <h2>{selectedListing.title}</h2>
+              <p>{selectedListing.price}/night</p>
+              <p>{selectedListing.city},{selectedListing.state},{selectedListing.zip}</p>
+            </div>
+          </Popup>
+        ) : null}
       </ReactMapGL>
-    /</div>
+      /
+    </div>
   );
 }
 function mapState(state) {
