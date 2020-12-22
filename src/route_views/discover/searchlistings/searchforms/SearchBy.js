912f3a1f6f4166035b4bbe81e55a275fb6812e27
tucker <tuckerwray84@gmail.com>
2020-12-22 18:35:13 -0500

    feat- SearchBy interface - four buttons select searchOptions - bootstraps a form and searchbar/button with reacthookforms - searchbar label /input matches styling of signup and login forms - label sits in middle of input until focused , then slides up into border

--- src/route_views/discover/searchlistings/searchforms/SearchBy.js
+++ src/route_views/discover/searchlistings/searchforms/SearchBy.js
@@ -1,27 +1,148 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-export default function SearchBy({setSearchResults}) {
+import { axiosWithAuth } from "../../../../utils/axiosWithAuth";
+import "./SearchBy.css";
+import { useForm } from "react-hook-form";
+
+export default function SearchBy({
+  setSearchResults,
+  searchResults,
+  searchDates,
+},props) {
   const [searchBy, setSearchBy] = useState("dates");
-  let startDate='2020-12-01';
-  let endDate='2020-12-06';
-  useEffect(() => {
-    axios
-      .post(`http://localhost:8001/api/listing/searchdates`,{startDate,endDate})
+  const [searchTerms, setSearchTerms] = useState("");
+  let today = new Date();
+  let todays_date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+
+  let start_date = searchDates?.start_date
+    ? searchDates.start_date
+    : `${todays_date}`;
+  let end_date = searchDates?.end_date
+    ? searchDates.end_date
+    : `${todays_date}`;
+  function handleChange(e) {
+    setSearchTerms(e.target.value);
+  }
+  function handleSelectSearchBy(e) {
+    setSearchBy(e.target.textContent);
+    document
+      .querySelectorAll("button.SearchBy")
+      .forEach((btn) => btn.classList.remove("active"));
+    e.target.classList.add("active");
+  }
+
+  function searchForListingsBy(event) {
+    event.preventDefault();
+    axiosWithAuth()
+      .post(`http://localhost:8001/api/search/${searchBy}`, {
+        start_date,
+        end_date,
+      })
       .then((res) => setSearchResults(res.data.listings))
       .catch((e) => console.error(e));
-  }, [searchBy]);
-  return (
-    <section>
-      <h2>set search option</h2>
-      <button onClick={(e) =>{ e.target.style.color=='indigo'?e.target.style.color=='white':e.target.style.color=='indigo'; setSearchBy("dates")}}>dates</button>
-      <button onClick={() => setSearchBy("price")}>price</button>
-      <button onClick={() => setSearchBy("location")}>location</button>
-      <button onClick={() => setSearchBy("amenities")}>amenities</button>
+    console.log({ searchBy }, { searchResults });
+  }
 
-      {searchBy === "date" && <div> "date" </div>}
-      {searchBy === "amenities" && <div> "amenities"</div>}
-      {searchBy === "location" && <div> "location" </div>}
-      {searchBy === "price" && <div> "price" </div>}
-    </section>
+  const { register, handleSubmit, errors, formState } = useForm({
+    defaultValues: { username: "", email: "", password: "", terms: false },
+  });
+
+  const [submitting, setSubmitting] = useState(false);
+  const { dirtyFields } = formState;
+
+  return (
+    <>
+      <section
+        style={{ width: "100vw", display: "flex", flexDirection: "column" }}
+      >
+        <form
+          style={{ fontSize: "4rem" }}
+          onSubmit={handleSubmit(async (formData) => {
+            setSubmitting(true);
+            try {
+              let { username, password, email } = formData;
+
+              props.login(formData, history);
+            } catch (error) {
+              console.error(error);
+            }
+            setSubmitting(false);
+          })}
+        >
+          <div className="input">
+            {dirtyFields.search ? (
+              <label className="isDirty" htmlFor="search">
+                search
+              </label>
+            ) : (
+              <label htmlFor="search">search</label>
+            )}
+            <input
+              autoComplete="off"
+              type="search"
+              name="search"
+              id="search"
+              ref={register({ required: "required" })}
+            />
+            {errors.search ? <p>search required</p> : null}
+          </div>
+          <div className="button">
+            <button type="submit" disabled={props.isLoading}>
+              find listings!
+              {props.isLoading && <span>{<LoadingClackers />} </span>}
+            </button>
+          </div>
+        </form>
+        <div
+          style={{
+            display: "flex",
+            width: "300px",
+            transition: "all 1s",
+            backgroundColor: "darkgray",
+            color: "indigo",
+            borderRadius: "5px",
+            textAlign: "center",
+            fontSize: "2.5rem",
+            fontWeight: "800",
+            textShadow: ` .5px .5px 0.1px black,
+            -.5px -.5px  .1px white,
+            .5px -.5px 1px black,
+            -.5px -.5px  1px white`,
+            boxShadow:
+              "inset 1px 1px 1px 1px white,inset -1px -1px 1px 1px white,-1px -1px 1px 1px black,0px 15px 15px -15px grey",
+            marginBottom: "1rem",
+          }}
+        >
+          <h2 style={{ width: "125px", textAlign: "left", padding: "0 1rem" }}>
+            search by:
+          </h2>
+
+          <p> - {searchBy} - </p>
+        </div>
+        <div style={{ width: "100vw", display: "flex", height: "40px" }}>
+          <button className="SearchBy" onClick={(e) => handleSelectSearchBy(e)}>
+            dates
+          </button>
+          <button className="SearchBy" onClick={(e) => handleSelectSearchBy(e)}>
+            price
+          </button>
+          <button className="SearchBy" onClick={(e) => handleSelectSearchBy(e)}>
+            location
+          </button>
+          <button className="SearchBy" onClick={(e) => handleSelectSearchBy(e)}>
+            amenities
+          </button>
+        </div>
+      </section>
+    </>
   );
+}
+
+{
+  /**
+
+add submit button on prps object?
+
+*/
 }
