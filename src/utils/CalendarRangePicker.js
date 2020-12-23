c76b3c33b042a76f2afb3fcd7d8df53d709d8d76
tucker <tuckerwray84@gmail.com>
2020-12-22 22:17:05 -0500

    feat-- CalendarRangePicker - passes SearchTerms State and StateSetter hooks down into CalendarRangePicker - sets SearchTerms state if endDate is updatedand both startDate&&endDate are truthy

--- src/utils/CalendarRangePicker.js
+++ src/utils/CalendarRangePicker.js
@@ -3,21 +3,21 @@ import { enGB } from "date-fns/locale";
 import { DateRangePicker, START_DATE, END_DATE } from "react-nice-dates";
 import "react-nice-dates/build/style.css";
 
-export default function CalendarRangePicker() {
+export default function CalendarRangePicker({searchTerms,setSearchTerms}) {
   const [startDate, setStartDate] = useState();
   const [endDate, setEndDate] = useState();
-
   useEffect(() => {
-    // let date = new Date(startDate);
-    // let dateString = new Date(date.getTime() - (date.getTimezoneOffset() * 60000 ))
-    //                     .toISOString()
-    //                     .split("T")[0];
-    //                     console.log({dateString})
-startDate&&
+if(startDate&&endDate){
 
-    console.log("satrtDate.toISOstring().split('T')[0]",startDate.toISOString().split('T')[0])
-  }, [endDate]);
-  
+setSearchTerms({
+
+  start_date:startDate?.toISOString().split('T')[0],end_date:endDate?.toISOString().split('T')[0]
+})
+console.log("satrtDate.toISOstring().split('T')[0]",startDate.toISOString().split('T')[0])
+} 
+}
+   , [endDate]);
+console.log({searchTerms})
   return (
     <DateRangePicker
       startDate={startDate}
