a9a3a87ed727b743d83665f11a35a1fb1dac7a53
tucker <tuckerwray84@gmail.com>
2020-12-23 12:12:34 -0500

    feat-- Price Slider - destructures searchTerms and setSearchTerms from props object - adds onChange prop to Slider with callback setSearchTerms - passes value into anon functin onChange Callback - calls effect hook to watch changes in value or searchTerms and log them to console

--- src/utils/RangeSlider.js
+++ src/utils/RangeSlider.js
@@ -1,12 +1,18 @@
 import react, { useEffect,useState } from "react";
 import Slider from "react-smooth-range-input";
 
-const RangeSlider = ({ value, min, max }) => {
+const RangeSlider = ({ value, min, max ,searchTerms,setSearchTerms}) => {
 
-
+    
+    const[rvalue,setValue]=useState()
+  useEffect(() => {
+    if (value) {
+      console.log({ value },{searchTerms});
+    }
+  }, [value,searchTerms]);
   return (
     <>
-      <Slider value={value} min={min} max={max} />
+      <Slider value={value} min={min} max={max} onChange={(value)=>{setSearchTerms(value)}}/>
 
     </>
   );
