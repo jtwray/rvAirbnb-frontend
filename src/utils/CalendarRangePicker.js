51b6646d00aec196d0038afd06e6340739b2ebe6
tucker <tuckerwray84@gmail.com>
2020-12-22 21:16:06 -0500

    feat-- CalendarRangePicker - rangepicker library from React real-nice-dates -start date and end date State hooks - converts startdate to backend acceptable form and logs to console on update :

---
+++ src/utils/CalendarRangePicker.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import { enGB } from "date-fns/locale";
+import { DateRangePicker, START_DATE, END_DATE } from "react-nice-dates";
+import "react-nice-dates/build/style.css";
+
+export default function CalendarRangePicker() {
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+
+  useEffect(() => {
+    // let date = new Date(startDate);
+    // let dateString = new Date(date.getTime() - (date.getTimezoneOffset() * 60000 ))
+    //                     .toISOString()
+    //                     .split("T")[0];
+    //                     console.log({dateString})
+startDate&&
+
+    console.log("satrtDate.toISOstring().split('T')[0]",startDate.toISOString().split('T')[0])
+  }, [endDate]);
+  
+  return (
+    <DateRangePicker
+      startDate={startDate}
+      endDate={endDate}
+      onStartDateChange={setStartDate}
+      onEndDateChange={setEndDate}
+      minimumDate={new Date()}
+      minimumLength={1}
+      format="yyyyMMdd"
+      locale={enGB}
+    >
+      {({ startDateInputProps, endDateInputProps, focus }) => (
+        <div className="date-range">
+          <input
+            className={"input" + (focus === START_DATE ? " -focused" : "")}
+            {...startDateInputProps}
+            placeholder="Start date"
+          />
+          <span className="date-range_arrow" />
+          <input
+            className={"input" + (focus === END_DATE ? " -focused" : "")}
+            {...endDateInputProps}
+            placeholder="End date"
+          />
+        </div>
+      )}
+    </DateRangePicker>
+  );
+}
+
+
+{/**
+
+step1:
+logout startDate and endDate to console
+step2:
+save to a state OBject
+step3:
+makesure its in the correct format to go to backend yyyymmdd
+step4:
+pass the stateoBject to the backend function
+step5:
+setup insubmit button to all axiosWithAUth to backend server with stateobject
+
+
+*/}
